deb97a8e97143e977369b895ec2b6592b455b269
[sivaranjani28] <[sivaranjani.saro@gmail.com]>
2016-08-17 14:44:50 +0530

    Added ValidationUtil.java

---
+++ OnlineBusTicketBooking/src/main/java/com/i2i/util/ValidationUtil.java
@@ -0,0 +1,18 @@
+package com.i2i.util;
+
+public class ValidationUtil {
+	
+	/**
+	 * <p>
+	 * Checks whether the given input is Zero or not
+	 * 
+	 * @param input
+	 *		input that is to be validated
+	 * @return boolean
+	 * 		true if input is not zero
+	 * 		false if input is zero
+	 */
+	public boolean checkIfZero (int input) {
+		return (input != 0);
+	}
+}
--- OnlineBusTicketBooking/src/main/java/com/i2i/webapp/controller/UserController.java
+++ OnlineBusTicketBooking/src/main/java/com/i2i/webapp/controller/UserController.java
@@ -8,7 +8,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
